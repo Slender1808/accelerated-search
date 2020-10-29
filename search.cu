08fc2c36653763847ffdf1a233f66702b2f9c949
Jadson G. Matos <50336793+Slender1808@users.noreply.github.com>
2020-10-28 23:57:11 -0300

    Create search.cu

---
+++ search.cu
@@ -0,0 +1,65 @@
+/*
+SPECIFICATIONS
+GPU Memory 1GB DDR3
+Memory Interface 128-bit
+Memory Bandwidth 29.0GB/s
+CUDA Cores 192
+Graphics APIs Shader Model 5.0, OpenGL 4.4, DirectX 11
+Compute APIs CUDA, DirectCompute,OpenCL
+*/
+
+/*
+Build
+nvcc -o teste.out teste.cu
+
+Teste
+sudo nvprof ./teste.bin
+*/
+
+#include <cuda.h>
+#include <stdio.h>
+#include <string>
+using namespace std;
+
+
+int main(void) {
+  // Estrutura
+  // Tamanho
+  // Dado
+
+
+  string data[4][2];
+
+  // dados
+  data[0][0] = "test";
+  data[1][0] = "casa";
+  data[2][0] = "gato";
+  data[3][0] = "foto";
+  // size
+  data[0][1] = "4";
+  data[1][1] = "4";
+  data[2][1] = "4";
+  data[3][1] = "4";
+
+  string alvo = "gato";
+
+  for(int i = 0; i < 4;i++){
+    // pegando tamanho do dado ta tabela
+    unsigned int sizeData = atoi(data[i][1].c_str());
+    // Verificando o tamanho dos dados
+    if(alvo.size() == sizeData){
+      // Verificação linear dos dados
+      for(int ind = 0; ind < sizeData; ){
+        if(alvo.at(ind) == data[i][0].at(ind)){
+          if(ind < sizeData - 1){
+            printf("%s \n", data[i][0].c_str());
+          }
+          i++;
+        } else {
+          ind = sizeData;
+        }
+      } 
+    }
+  }
+  return 0;
+}
